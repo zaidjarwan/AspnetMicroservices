6e1e7c5c5e3b1114c1b559331ddfba3613d1f72a
ramious76 <8411220+ramious76@users.noreply.github.com>
2021-12-05 15:15:39 -0800

    Modified BasketController to use MassTransit/RabbitMQ to send message to queue from new basket/checkout api.

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
-﻿using Basket.API.Entities;
+﻿using AutoMapper;
+using Basket.API.Entities;
 using Basket.API.GrpcServices;
 using Basket.API.Repsitories;
+using EventBus.Messages.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,12 +18,18 @@ namespace Basket.API.Controllers
     {
         private readonly IBasketRepository _basketRepository;
         private readonly DiscountGrpcService _discountGrpcService;
+        private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
         public BasketController(IBasketRepository basketRepository,
-                                DiscountGrpcService discountGrpcService)
+                                DiscountGrpcService discountGrpcService,
+                                IMapper mapper,
+                                IPublishEndpoint publishEndpoint)
         {
             this._basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
             this._discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
+            this._mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this._publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
         }
 
         [HttpGet("{userName}", Name ="GetBasket")]
@@ -36,9 +45,6 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
-            // TODO: Communicate with Discount.Grpc
-            // and calculate latest prices of products to shopping cart.
-
             foreach(var item in basket.Items)
             {
                 var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
@@ -56,6 +62,34 @@ namespace Basket.API.Controllers
             return Ok();
         }
 
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            // get existing basket with total price
+            // create basketCheckoutEvent  --  set totalPrice on basketCheckout eventMessage
+            // sent checkout event to rabbitmq
+            // remove the basket
 
+            // get existing basket with total price
+            var basket = await _basketRepository.GetBasket(basketCheckout.UserName);
+            if(basket == null) 
+            { 
+                return BadRequest(); 
+            }
+
+            // sent checkout event to rabbitmq
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMessage);
+
+
+            // remove the basket
+            await _basketRepository.DeleteBasket(basket.UserName);
+
+            return Accepted();
+        }
     }
 }
